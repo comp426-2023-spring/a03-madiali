bc3806a5588abccd2185204186d0ab14d0e36bd5
Madison Lester <madiali@ad.unc.edu>
2023-03-08 14:20:38 -0500

    Progress

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -9,7 +9,7 @@ export function rps(playerShot) {
     if (playerShot !== undefined) {
         playerShot = playerShot.toLowerCase()
     }
-    let opponentShot = validShots[Math.random() * 3]
+    
 
     if (playerShot === undefined) {
         return JSON.stringify({'player': opponentShot})
